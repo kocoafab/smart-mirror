c5464a69bbe63c65d2dbce5f6ac175a3bb1b5829
kocoafab <kocoafab@kocoa.or.kr>
2016-03-03 14:21:07 +0900

    initial version

---
+++ README.md
@@ -0,0 +1,130 @@
+<<<<<<< HEAD
+# Smart Mirror
+
+[![Join the chat at https://gitter.im/evancohen/smart-mirror](https://badges.gitter.im/evancohen/smart-mirror.svg)](https://gitter.im/evancohen/smart-mirror?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+This project was inspired by [HomeMirror](https://github.com/HannahMitt/HomeMirror) and Michael Teeuw's [Magic Mirror](http://michaelteeuw.nl/tagged/magicmirror). It uses [annyang](https://github.com/TalAter/annyang) for voice interactivity, [electron](http://electron.atom.io/) to make it cross platform, and integrates with Philips Hue. It is my own take on what a "smart mirror" can be.
+
+[See it in action (Video)](https://www.youtube.com/watch?v=PDIbhV8Nvq8)
+
+#### Why start from scratch?
+Starting from scratch was less about other projects not being good enough and more about my own learning experience. While I did get a lot of inspiration from other projects I really wanted to see how much further I could take things.
+
+#### Gitter:
+A live chat to get help and discuss mirror related issues: https://gitter.im/evancohen/smart-mirror. Usually there are a few folks hanging around in the lobby, but if there arent you are probubly better off [filing an issue](https://github.com/evancohen/smart-mirror/issues/new).
+
+### Getting Started
+#### Hardware Components
+- Raspberry Pi 2**
+- USB Microphone (Or Webcam w/ microphone)
+- Monitor (with the bezel removed)
+- Mirror Pane (aka Observation Glass)
+- Philips Hue
+
+** Also compatible with other Linux, Windows, and OSX devices. See the `cordova` branch for Android and iOS compatibility.
+
+#### Installation
+In order to get started I suggest a clean install of Raspbian. You can snag a fresh copy of Jessie (recommended, it's the future) or Wheezy from the [Raspbian Download Page](https://www.raspberrypi.org/downloads/raspbian/).
+
+You'll also need to install Node (v4.0.0+) which now comes bundled with npm.
+```
+wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv7l.tar.gz 
+tar -xvf node-v4.0.0-linux-armv7l.tar.gz 
+cd node-v4.0.0-linux-armv7l
+```
+Copy to /usr/local
+```
+sudo cp -R * /usr/local/
+```
+
+##### Getting the code
+Next up you'll want to clone this repository into your user's home folder on your Pi:
+```
+cd ~
+git clone https://github.com/evancohen/smart-mirror.git
+```
+
+##### Configuring the mirror
+You'll need to fill in a few things into `config.js`, which should end up looking something like this:
+``` javascript
+var config = {
+    lenguage : "en",
+    greeting : ["Hi, sexy!", "Hey There!", "Looking Awesome!"],
+    forcast : {
+        key : "a6s5dg39j78qj38sjs91je9djadfa1e",
+        units : "auto"
+    },
+    hue : {
+        ip : "192.168.1.99",
+        uername : "as9234ho0dfhoq01f2as3yh4m0",
+        group : "0",
+    },
+    calendar: {
+      icals : ["https://calendar.google.com/calendar/ical/SOMESTUFF/basic.ics",
+"https://outlook.office365.com/owa/calendar/SOMESTUFF/reachcalendar.ics"],
+      maxResults: 9,
+      maxDays: 365
+    },
+    giphy: {
+      key : "a6s5dg39j78qj38sjs91je9djadfa1e"
+    },
+    traffic: {
+      key : "a6s5dg39j78qj38sjs91je9djadfa1e",
+      mode : "Driving",
+      origin : "350 5th Ave, New York, NY 10118",
+      destination : "1 Dr Carlton B Goodlett Pl, San Francisco, CA 94102",
+      name : "work",
+      reload_interval : 5
+    }
+}
+```
+Note that if you start the mirror and get a black screen you most likeley have an issue with your config.
+
+##### Configuring the Pi
+In order to rotate your monitor you'll need to add the following line to `/boot/config.txt`
+```
+display_rotate=1
+```
+You can also set this value to '3' to have a flipped vertical orientation.
+
+In order to disable the screensaver you'll want to comment out (with a '#') the `@xscreensaver` and `@lxpanel` lines in `/etc/xdg/lxsession/LXDE/autostart`. You'll also want to add the following lines to that same file
+```
+@xset s off
+@xset -dpms
+@xset s noblank
+```
+
+##### Install dependencies and run
+Before we can run the thing we've got to install the projects dependencies. From the root of the `smart-mirror` directory run:
+```
+npm install
+```
+
+This will take a minute, it has to download [electron-prebuilt](https://github.com/mafintosh/electron-prebuilt). Once that is done you can launch the mirror with
+```
+npm start
+```
+
+#### Development
+To launch the mirror with a debug window attached use the following command:
+```
+npm start dev
+```
+More info coming soon(ish). In the meantime head over to the [gitter chat](https://gitter.im/evancohen/smart-mirror) for help. 
+
+#### Troubleshooting
+If you are having trouble getting a USB microphone to work on your Pi try following [these steps](https://github.com/evancohen/smart-mirror/issues/20)
+
+### License
+MIT
+
+### Author
+[Evan Cohen](http://evanbtcohen.com/)
+
+### More info
+Favicon from [In the Wake of the King](http://walkingmind.evilhat.com/2014/03/17/in-the-wake-of-the-king/), a head nod to **The Watcher** – "A byblow of the king and a queen of the sea, she has remained apart from the workings of her family, more home beneath the waves, watching all through water and mirror. Her ambitions lie outside the Eternal Kingdom, but her secrets are valuable everywhere."
+
+Awesome.
+=======
+# smart-mirror
+>>>>>>> d95c3ae4567ba3a4ead692dd408ffbef158ce510
---
+++ js/controller.js
@@ -0,0 +1,229 @@
+(function(angular) {
+    'use strict';
+
+    function MirrorCtrl(
+            AnnyangService,
+            GeolocationService,
+            WeatherService,
+            MapService,
+            HueService,
+            CalendarService,
+            XKCDService,
+            GiphyService,
+            TrafficService,
+            $scope, $timeout, $interval) {
+        var _this = this;
+        var DEFAULT_COMMAND_TEXT = 'Say "What can I say?" to see a list of commands...';
+        $scope.listening = false;
+        $scope.debug = false;
+        $scope.focus = "default";
+        $scope.user = {};
+        $scope.interimResult = DEFAULT_COMMAND_TEXT;
+
+        //Update the time
+        function updateTime(){
+            $scope.date = new Date();
+        }
+
+        // Reset the command text
+        var restCommand = function(){
+          $scope.interimResult = DEFAULT_COMMAND_TEXT;
+        }
+
+        _this.init = function() {
+            var tick = $interval(updateTime, 1000);
+            updateTime();
+            GeolocationService.getLocation({enableHighAccuracy: true}).then(function(geoposition){
+                console.log("Geoposition", geoposition);
+                $scope.map = MapService.generateMap(geoposition.coords.latitude+','+geoposition.coords.longitude);
+            });
+            restCommand();
+
+            var refreshMirrorData = function() {
+                //Get our location and then get the weather for our location
+                GeolocationService.getLocation({enableHighAccuracy: true}).then(function(geoposition){
+                    console.log("Geoposition", geoposition);
+                    WeatherService.init(geoposition).then(function(){
+                        $scope.currentForcast = WeatherService.currentForcast();
+                        $scope.weeklyForcast = WeatherService.weeklyForcast();
+                        $scope.hourlyForcast = WeatherService.hourlyForcast();
+                        console.log("Current", $scope.currentForcast);
+                        console.log("Weekly", $scope.weeklyForcast);
+                        console.log("Hourly", $scope.hourlyForcast);
+                    });
+                }, function(error){
+                    console.log(error);
+                });
+
+                CalendarService.getCalendarEvents().then(function(response) {
+                    $scope.calendar = CalendarService.getFutureEvents();
+                }, function(error) {
+                    console.log(error);
+                });
+
+                $scope.greeting = config.greeting[Math.floor(Math.random() * config.greeting.length)];
+            };
+
+            refreshMirrorData();
+            $interval(refreshMirrorData, 3600000);
+
+            var refreshTrafficData = function() {
+                TrafficService.getTravelDuration().then(function(durationTraffic) {
+                    console.log("Traffic", durationTraffic);
+                    $scope.traffic = {
+                        destination:config.traffic.name,
+                        hours : durationTraffic.hours(),
+                        minutes : durationTraffic.minutes()
+                    };
+                }, function(error){
+                    $scope.traffic = {error: error};
+                });
+            };
+
+            refreshTrafficData();
+            $interval(refreshTrafficData, config.traffic.reload_interval * 60000);
+
+            var defaultView = function() {
+                console.debug("Ok, going to default view...");
+                $scope.focus = "default";
+            }
+
+            // List commands
+            AnnyangService.addCommand('What can I say', function() {
+                console.debug("Here is a list of commands...");
+                console.log(AnnyangService.commands);
+                $scope.focus = "commands";
+            });
+
+            // Go back to default view
+            AnnyangService.addCommand('Go home', defaultView);
+
+            // Hide everything and "sleep"
+            AnnyangService.addCommand('Go to sleep', function() {
+                console.debug("Ok, going to sleep...");
+                $scope.focus = "sleep";
+            });
+
+            // Go back to default view
+            AnnyangService.addCommand('Wake up', defaultView);
+
+            // Hide everything and "sleep"
+            AnnyangService.addCommand('Show debug information', function() {
+                console.debug("Boop Boop. Showing debug info...");
+                $scope.debug = true;
+            });
+
+            // Hide everything and "sleep"
+            AnnyangService.addCommand('Show map', function() {
+                console.debug("Going on an adventure?");
+                GeolocationService.getLocation({enableHighAccuracy: true}).then(function(geoposition){
+                    console.log("Geoposition", geoposition);
+                    $scope.map = MapService.generateMap(geoposition.coords.latitude+','+geoposition.coords.longitude);
+                    $scope.focus = "map";
+                });
+             });
+
+	    AnnyangService.addCommand('weather', function() {
+		console.debug("Show Weather!");
+		$scope.focus = "weather";
+            });
+
+
+            // Hide everything and "sleep"
+            AnnyangService.addCommand('Show (me a) map of *location', function(location) {
+                console.debug("Getting map of", location);
+                $scope.map = MapService.generateMap(location);
+                $scope.focus = "map";
+            });
+
+            // Zoom in map
+            AnnyangService.addCommand('(map) zoom in', function() {
+                console.debug("Zoooooooom!!!");
+                $scope.map = MapService.zoomIn();
+            });
+
+            AnnyangService.addCommand('(map) zoom out', function() {
+                console.debug("Moooooooooz!!!");
+                $scope.map = MapService.zoomOut();
+            });
+
+            AnnyangService.addCommand('(map) zoom (to) *value', function(value) {
+                console.debug("Moooop!!!", value);
+                $scope.map = MapService.zoomTo(value);
+            });
+
+            AnnyangService.addCommand('(map) reset zoom', function() {
+                console.debug("Zoooommmmmzzz00000!!!");
+                $scope.map = MapService.reset();
+                $scope.focus = "map";
+            });
+
+            // Search images
+            AnnyangService.addCommand('Show me *term', function(term) {
+                console.debug("Showing", term);
+            });
+
+            // Change name
+            AnnyangService.addCommand('My (name is)(name\'s) *name', function(name) {
+                console.debug("Hi", name, "nice to meet you");
+                $scope.user.name = name;
+            });
+
+            // Set a reminder
+            AnnyangService.addCommand('Remind me to *task', function(task) {
+                console.debug("I'll remind you to", task);
+            });
+
+            // Clear reminders
+            AnnyangService.addCommand('Clear reminders', function() {
+                console.debug("Clearing reminders");
+            });
+
+            // Check the time
+            AnnyangService.addCommand('what time is it', function(task) {
+                 console.debug("It is", moment().format('h:mm:ss a'));
+		$scope.focus = "time";
+            });
+
+            // Turn lights off
+            AnnyangService.addCommand('(turn) (the) :state (the) light(s) *action', function(state, action) {
+                HueService.performUpdate(state + " " + action);
+            });
+
+            //Show giphy image
+            AnnyangService.addCommand('giphy *img', function(img) {
+                GiphyService.init(img).then(function(){
+                    $scope.gifimg = GiphyService.giphyImg();
+                    $scope.focus = "gif";
+                });
+            });
+
+            // Show xkcd comic
+            AnnyangService.addCommand('Show xkcd', function(state, action) {
+                console.debug("Fetching a comic for you.");
+                XKCDService.getXKCD().then(function(data){
+                    $scope.xkcd = data.img;
+                    $scope.focus = "xkcd";
+                });
+            });
+
+            var resetCommandTimeout;
+            //Track when the Annyang is listening to us
+            AnnyangService.start(function(listening){
+                $scope.listening = listening;
+            }, function(interimResult){
+                $scope.interimResult = interimResult;
+                $timeout.cancel(resetCommandTimeout);
+            }, function(result){
+                $scope.interimResult = result[0];
+                resetCommandTimeout = $timeout(restCommand, 5000);
+            });
+        };
+
+        _this.init();
+    }
+
+    angular.module('SmartMirror')
+        .controller('MirrorCtrl', MirrorCtrl);
+
+}(window.angular));
